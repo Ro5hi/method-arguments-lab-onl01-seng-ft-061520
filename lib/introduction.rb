5f3d78324d005d81d13bbeff55cbd638506a8f0d
beautiful-instructions-5625 <slen.detablan@gmail.com>
2020-06-15 19:01:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,10 +5,6 @@ def introduction(name)
   puts "Hi, my name is #{name}.}"
 end
 
-def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
-
 introduction("Josh")
 # > Hi, my name is Josh.
 
